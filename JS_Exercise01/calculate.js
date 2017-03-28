6fcd86226ca33f307da16e89ee9fcdb1ae0d0669
906 <906@TOPAZ15>
2017-03-28 11:52:07 +0300

    Add project files.

---
+++ JS_Exercise01/calculate.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    var number = document.getElementById("txtNumber").value;
+
+    function claculate(event) {
+        var slectedOperator = event.target.value;
+        switch (switch_on) {
+            default:
+
+        }
+        return 
+    };
+
+    document.addEventListener("DOMContentLoaded", function () {
+        var plus = document.getElementById("btnPlus");
+        var minus = document.getElementById("btnMinus");
+        var mult = document.getElementById("btnMult");
+        var divide = document.getElementById("btnDivide");
+
+        plus.addEventListener("click", calculate);
+        minus.addEventListener("click", calculate);
+        mult.addEventListener("click", calculate);
+        divide.addEventListener("click", calculate);
+    });
+})();
+
